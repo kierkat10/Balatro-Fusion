5f1b8d771b65377c2c063ef5ab1abd83bf6f678d
Lact4 <103157629+marcoosgiam@users.noreply.github.com>
2025-12-19 15:52:21 -0300

    2 new riff-raff

---
+++ items/joker/juggling_juggler.lua
@@ -0,0 +1,56 @@
+
+SMODS.BalatroFusion.Fusion:new_generic({
+    id = "joker_fusion",
+    key = "juggling_juggler",
+    name = "Juggling Juggler",
+    input = {
+        "j_riff_raff",
+        "j_juggler",
+    },
+    output = "j_bfs_juggling_juggler"
+})
+
+SMODS.Joker {
+    key = "juggling_juggler",
+    name = "Juggling Juggler",
+    config = {
+        extra={
+            immutable={
+                hand_size=0,
+                hand_size_per_common_joker=1,
+            },
+        },
+    },
+    pos = {x=7,y=8,},
+    cost = 9,
+    rarity = "bfs_fused",
+    blueprint_compat = true,
+    atlas = "riff-raff",
+    update = function (self,card)
+        local common_jokers_owned = 0
+        if G.jokers then
+            for i,v in ipairs(G.jokers.cards) do
+                if v:is_rarity("Common") then common_jokers_owned=common_jokers_owned+1 end
+            end
+        end
+        G.hand.config.card_limit=G.hand.config.card_limit-card.ability.extra.immutable.hand_size
+        card.ability.extra.immutable.hand_size = card.ability.extra.immutable.hand_size_per_common_joker*common_jokers_owned
+        G.hand.config.card_limit=G.hand.config.card_limit+card.ability.extra.immutable.hand_size
+    end,
+    remove_from_deck = function (self,card)
+        G.hand:change_size(-card.ability.extra.immutable.hand_size)
+    end,
+    loc_vars = function (self,info_queue,card)
+       return {
+        vars={
+            card.ability.extra.immutable.hand_size_per_common_joker,
+            card.ability.extra.immutable.hand_size,
+        }
+       } 
+    end,
+    bfs_credits = {
+        idea = { "StellarBlue" },
+        art = {"StellarBlue"},
+        code = { "Lact4???"}
+    },
+}
---
+++ items/joker/sock_my_buskin.lua
@@ -0,0 +1,56 @@
+
+SMODS.BalatroFusion.Fusion:new_generic({
+    id = "joker_fusion",
+    key = "sock_my_buskin",
+    name = "Sock My Buskin",
+    input = {
+        "j_riff_raff",
+        "j_sock_and_buskin",
+    },
+    output = "j_bfs_sock_my_buskin"
+})
+
+SMODS.Joker {
+    key = "sock_my_buskin",
+    name = "Sock My Buskin",
+    config = {
+        extra={
+            immutable={retriggers_per_common_joker=1,repetitions=1,},
+        },
+    },
+    pos = {x=9,y=10,},
+    cost = 11,
+    rarity = "bfs_fused",
+    blueprint_compat = true,
+    atlas = "riff-raff",
+    calculate = function (self,card,context)
+        if context.repetition and context.cardarea == G.play and context.other_card:is_face() then
+            return {
+                repetitions=card.ability.extra.immutable.repetitions,
+                message = localize('k_again_ex')
+            }
+        end
+    end,
+    update = function (self,card)
+        local common_jokers_owned = 0
+        if G.jokers then
+            for i,v in ipairs(G.jokers.cards) do
+                if v:is_rarity("Common") then common_jokers_owned=common_jokers_owned+1 end
+            end
+        end
+        card.ability.extra.immutable.repetitions=1+(card.ability.extra.immutable.retriggers_per_common_joker*common_jokers_owned)
+    end,
+    loc_vars = function (self,info_queue,card)
+       return {
+        vars={
+            card.ability.extra.immutable.retriggers_per_common_joker,
+            card.ability.extra.immutable.repetitions,
+        }
+       } 
+    end,
+    bfs_credits = {
+        idea = { "StellarBlue" },
+        art = {"StellarBlue"},
+        code = { "Lact4???"}
+    },
+}
